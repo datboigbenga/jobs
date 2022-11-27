685c1ad01b0ff3fdfff2226d9270c6d15c825c5f
datboigbenga <eruolagbenga@gmail.com>
2022-11-27 03:24:15 -0800

    Removed files and folders

--- middleware/notFound.js
+++
@@ -1,6 +0,0 @@
-const {StatusCodes} = require("http-status-codes")
-const notFound = (req, res)=>{
-    res.status(StatusCodes.NOT_FOUND).json({msg: "Page not Found"})
-}
-
-module.exports = notFound
